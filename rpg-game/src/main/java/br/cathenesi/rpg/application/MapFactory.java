90dc3af578f21aa2b5e9ce2825d15651da052e23
Carlos Athenesi <carlos.athenesi@spcbrasil.org.br>
2018-11-25 23:16:55 -0200

    Adjusting factory

--- rpg-game/src/main/java/br/cathenesi/rpg/application/MapFactory.java
+++ rpg-game/src/main/java/br/cathenesi/rpg/application/MapFactory.java
@@ -1,20 +1,24 @@
 package br.cathenesi.rpg.application;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.nio.charset.StandardCharsets;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.stream.Stream;
 
 import br.cathenesi.rpg.domain.model.gamemap.GameMap;
 
 public class MapFactory {
 
 	class MapElement {
-		
+
 		private String key;
 		private String value;
 
@@ -34,32 +38,38 @@ public class MapFactory {
 
 	public List<GameMap> createMaps() throws Exception {
 
+		List<MapElement> mapElements = new ArrayList<>();
 		List<GameMap> maps = new ArrayList<>();
 
-		File root = new File(ClassLoader.getSystemResource("maps").getFile());
-		File[] files = root.listFiles(new FileFilter() {
+		URI uri = this.getClass().getResource("/maps").toURI();
+		Path myPath;
+		if (uri.getScheme().equals("jar")) {
+			FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.<String, Object>emptyMap());
+			myPath = fileSystem.getPath("/maps");
+		} else {
+			myPath = Paths.get(uri);
+		}
 
-			public boolean accept(File pathname) {
-				return pathname.getName().endsWith(".yml");
-			}
-		});
+		try (Stream<Path> walk = Files.walk(myPath, 1)) {
+			
+			for (Iterator<Path> it = walk.iterator(); it.hasNext();) {
+				Path path = it.next();
+				if (path.toString().endsWith(".yml")) {
 
-		List<MapElement> mapElements = new ArrayList<>();
+					Files.lines(path).forEach(s -> {
+						StringTokenizer st = new StringTokenizer(s, ":");
+						if (st.countTokens() == 2) {
+							mapElements.add(new MapElement(st.nextToken().trim(), st.nextToken().trim()));
+						}
+					});
 
-		for (File file : files) {
-
-			Files.lines(Paths.get(file.getAbsolutePath()), StandardCharsets.UTF_8).forEach(s -> {
-				StringTokenizer st = new StringTokenizer(s, ":");
-				if (st.countTokens() == 2) {
-					mapElements.add(new MapElement(st.nextToken().trim(), st.nextToken().trim()));
+					maps.add(new MapBuilder(mapElements).build());
+					mapElements.clear();
 				}
-			});
-
-			maps.add(new MapBuilder(mapElements).build());
-			mapElements.clear();
+			}
 		}
 
 		return maps;
 	}
-	
+
 }
